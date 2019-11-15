75939be37a9da4d7e2f32e9b16ddb9b6957571e4
Evan Elias <evan@skeema.net>
2019-11-15 13:02:50 -0500

    testing

--- employees/employees.sql
+++ employees/employees.sql
@@ -5,5 +5,6 @@ CREATE TABLE `employees` (
   `last_name` varchar(16) NOT NULL,
   `gender` char(1) DEFAULT NULL,
   `hire_date` date NOT NULL,
+  `nickname` varchar(30) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`emp_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
