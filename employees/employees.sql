306f93172613e484d4212c38c9a846c6f4a5c4e6
Evan Elias <evan@skeema.net>
2019-11-15 15:50:38 -0500

    add nickname col

--- employees/employees.sql
+++ employees/employees.sql
@@ -5,5 +5,6 @@ CREATE TABLE `employees` (
   `last_name` varchar(16) NOT NULL,
   `gender` char(1) DEFAULT NULL,
   `hire_date` date NOT NULL,
+  nickname varchar(60) character set utf8,
   PRIMARY KEY (`emp_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
